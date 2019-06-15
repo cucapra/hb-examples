9d5b078010cf0e491c946624197876976c366d20
Adrian Sampson <adrian@radbox.org>
2019-06-15 17:13:18 -0400

    Simplify error handling
    I'd *really* like to assuming that 0 indicates success...

--- noop/host.c
+++ noop/host.c
@@ -16,7 +16,7 @@ int main(int argc, const char **argv) {
     hb_mc_device_t device;
     hb_mc_dimension_t mesh_dim = {.x = 2, .y = 2};
     err = hb_mc_device_init(&device, "example", 0,  mesh_dim);
-    if (err != HB_MC_SUCCESS) {
+    if (err) {
         fprintf(stderr, "error in hb_mc_device_init\n");
         return err;
     }
@@ -24,7 +24,7 @@ int main(int argc, const char **argv) {
     // Load the RISC-V binary onto the device.
     // TK I don't know if the `alloc_name` parameter does anything important.
     err = hb_mc_device_program_init(&device, "noop.riscv", "example", 0);
-    if (err != HB_MC_SUCCESS) {
+    if (err) {
         fprintf(stderr, "error in hb_mc_device_program_init\n");
         return err;
     }
@@ -44,21 +44,21 @@ int main(int argc, const char **argv) {
     hb_mc_dimension_t grid_dim = {.x = 1, .y = 1};
     hb_mc_dimension_t tg_dim = {.x = 2, .y = 2};
     err = hb_mc_grid_init(&device, grid_dim, tg_dim, "noop", 0, NULL);
-    if (err != HB_MC_SUCCESS) {
+    if (err) {
         fprintf(stderr, "error in hb_mc_grid_init\n");
         return err;
     }
 
     // Run the program on the device.
     err = hb_mc_device_tile_groups_execute(&device);
-    if (err != HB_MC_SUCCESS) {
+    if (err) {
         fprintf(stderr, "error in hb_mc_device_tile_groups_execute\n");
         return err;
     }
 
     // Clean up.
     err = hb_mc_device_finish(&device);
-    if (err != HB_MC_SUCCESS) {
+    if (err) {
         fprintf(stderr, "error in hb_mc_device_finish\n");
         return err;
     }
