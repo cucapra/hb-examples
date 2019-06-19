48b7438538844f6de6954b329873554628dbec79
Alexa VanHattum <amv94@cornell.edu>
2019-06-19 14:29:09 -0400

    Separate out host/device communication surface for message example

--- message-communication/communication.c
+++ message-communication/communication.c
@@ -1,37 +1,12 @@
-#include "bsg_manycore.h"
-#include "bsg_set_tile_x_y.h"
-#include "bsg_cuda_lite_runtime.h"
-#include "bsg_tilegroup.h"
+#include <bsg_manycore.h>
+#include <bsg_set_tile_x_y.h>
+#include <bsg_cuda_lite_runtime.h>
 
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-// --- Communication buffer number of elements ---
-
-// NUM_COMMS should be defined from the compiler/Makefile
-#ifndef NUM_COMMS
-#define NUM_COMMS 100
-#endif // NUM_COMMS
-
-#ifndef COMMS_BUFFER_SIZE
-#define COMMS_BUFFER_SIZE NUM_COMMS * sizeof(long long)
-#endif // NUM_COMMS
-
-// --- Communication buffer metadata ---
-volatile int32_t comms_ready[NUM_COMMS];     // Communication data for the ID is ready to be read
-volatile int32_t comms_start_idx[NUM_COMMS]; // Start index into the data buffer
-volatile int32_t comms_size[NUM_COMMS];      // Size of the value for this communication
-
-// --- Communication buffer ---
-// Data and metadata should be kept on the *receiving* tile's memory
-volatile char comms_buffer[COMMS_BUFFER_SIZE];
-
-// Treat the communication buffer as a linear buffer for now
-volatile int32_t comms_buffer_start = 0;
-
-// Return value. Note this attribute specifies that this is shared DRAM memory
-volatile int32_t global_return  __attribute__((section(".dram")));
+#include "bsg-device-communication.h"
 
 // Set up the completion barrier.
 #define BSG_TILE_GROUP_X_DIM bsg_tiles_X
@@ -39,72 +14,6 @@ volatile int32_t global_return  __attribute__((section(".dram")));
 #include "bsg_tile_group_barrier.h"
 INIT_TILE_GROUP_BARRIER(r_barrier, c_barrier, 0, bsg_tiles_X - 1, 0,
     bsg_tiles_Y - 1);
-
-
-void copy(char *dest, char *src, unsigned len) {
-    while (len) {
-        *dest = *src;
-        dest++;
-        src++;
-        len -= sizeof(char);
-    }
-}
-
-void send(void *value, int size, int to_core, int id, void *context) {
-    // Construct coordinates
-    int to_x = bsg_id_to_x(to_core);
-    int to_y = bsg_id_to_y(to_core);
-
-    // Grab the start of the communication buffer for the recepient
-    int start_idx;
-    bsg_remote_load(to_x, to_y, &comms_buffer_start, start_idx);
-
-    // Increment the recepient's buffer start by the size
-    bsg_remote_store(to_x, to_y, &comms_buffer_start, start_idx + size);
-
-    // Set the communication buffer metadata on the recepient for this ID
-    bsg_remote_store(to_x, to_y, &comms_start_idx[id], start_idx);
-    bsg_remote_store(to_x, to_y, &comms_size[id], size);
-
-    // Write the actual value into the communication buffer on the recepient
-    char *buffer_value_ptr = (void *)bsg_remote_ptr(to_x, to_y, &comms_buffer[start_idx]);    
-    copy(buffer_value_ptr, value, size);
-
-    // Finally, write out to the recepient that this value is ready
-    bsg_remote_store(to_x, to_y, &comms_ready[id], 1);
-}
-
-void *_receive_shared(int size, int from_core, int id, void *context) {
-    // Wait patiently until the value is ready
-    bsg_wait_while(!comms_ready[id]);
-
-    // Metdata should already be written by sender
-    int start_idx = comms_start_idx[id];
-    int actual_size = comms_size[id];
-
-    // Sanity check sizes match
-    if (actual_size != size) {
-        // TODO: error on device
-    }
-
-    // Return this address into the buffer
-    return &comms_buffer[start_idx];
-}
-
-void *receive(int size, int from_core, int id, void *context) {
-    void *value = _receive_shared(size, from_core, id, context);
-
-    // Regular reads should be destructive: the value is no longer ready
-    comms_ready[id] = 0;
-
-    // Return this address into the buffer
-    return value;
-}
-
-void *receive_argument(int size, int from_core, int id, void *context) {
-    // Argument reads should not be destructive
-    return _receive_shared(size, from_core, id, context);;   
-}
 
 int communicate(int32_t *src, int32_t *dest) {
 
@@ -114,15 +23,14 @@ int communicate(int32_t *src, int32_t *dest) {
     int tile_id = bsg_x_y_to_id(bsg_x, bsg_y);  
 
     int int_size = sizeof(int32_t);
-    int32_t host_id = -1;
     int32_t value, valueFrom0, valueFrom1, valueFrom2;
     switch(tile_id) {
         case 0:
             // Receive a value from the host
-            value = *(int32_t *)receive_argument(int_size, host_id, tile_id, NULL);
+            value = *(int32_t *)receive_argument(int_size, tile_id, NULL);
 
             // Because the value is an argument, we should be able to read it twice
-            value = *(int32_t *)receive_argument(int_size, host_id, tile_id, NULL);
+            value = *(int32_t *)receive_argument(int_size, tile_id, NULL);
 
             // Forward this value to tile 3, communication ID 4
             send(&value, int_size, 3, 4, NULL);
@@ -130,7 +38,7 @@ int communicate(int32_t *src, int32_t *dest) {
 
         case 1:
             // Receive a value from the host
-            value = *(int32_t *)receive_argument(int_size, host_id, tile_id, NULL);
+            value = *(int32_t *)receive_argument(int_size, tile_id, NULL);
 
             // Forward this value to tile 3, communication ID 5
             send(&value, int_size, 3, 5, NULL);
@@ -138,7 +46,7 @@ int communicate(int32_t *src, int32_t *dest) {
 
         case 2:
             // Receive a value from the host
-            value = *(int32_t *)receive_argument(int_size, host_id, tile_id, NULL);
+            value = *(int32_t *)receive_argument(int_size, tile_id, NULL);
 
             // Forward this value to tile 3, communication ID 6
             send(&value, int_size, 3, 6, NULL);
@@ -146,7 +54,7 @@ int communicate(int32_t *src, int32_t *dest) {
 
         default:
             // Receive a value from the host
-            value = *(int32_t *)receive_argument(int_size, host_id, tile_id, NULL);
+            value = *(int32_t *)receive_argument(int_size, tile_id, NULL);
 
             // Receive forwarded values from other tiles
             valueFrom0 = *(int32_t *)receive(int_size, 0, 4, NULL);
@@ -154,7 +62,8 @@ int communicate(int32_t *src, int32_t *dest) {
             valueFrom2 = *(int32_t *)receive(int_size, 0, 6, NULL);
 
             // Should be 0 + 1 + 2 + 3 = 6
-            global_return = value + valueFrom0 + valueFrom1 + valueFrom2;
+            value = value + valueFrom0 + valueFrom1 + valueFrom2;
+            send_return(&value, int_size, NULL);
             break;
     }
 
--- message-communication/host.c
+++ message-communication/host.c
@@ -3,13 +3,11 @@
 
 #include <bsg_manycore_cuda.h>
 #include <bsg_manycore_loader.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int _load_symbol_to_eva(hb_mc_device_t *device, const char *symbol, hb_mc_eva_t *eva) {
-    return hb_mc_loader_symbol_to_eva(device->program->bin, device->program->bin_size, 
-        symbol, eva);     
-}
+#include "bsg-host-communication.h"
 
 int do_communication(int32_t *dest) {
     int err;
@@ -32,82 +30,18 @@ int do_communication(int32_t *dest) {
     err = hb_mc_grid_init(&device, grid_dim, tg_dim, "communicate", 0, NULL);
     if (err) return err;
 
-    // Look up the communication buffer and metadata symbols. "EVA" is for 
-    // "endpoint virtual address," and it represents an address in the device's
-    // memory. An EVA is simply backed by a uint32_t, so we can do standard
-    // pointer-type arithmetic on it to access array memory.
-    hb_mc_eva_t buffer_eva, ready_eva, start_idx_eva, size_eva, start_eva; 
-    err = _load_symbol_to_eva(&device, "comms_buffer", &buffer_eva); 
-    err |= _load_symbol_to_eva(&device, "comms_ready", &ready_eva); 
-    err |= _load_symbol_to_eva(&device, "comms_start_idx", &start_idx_eva); 
-    err |= _load_symbol_to_eva(&device, "comms_size", &size_eva); 
-    err |= _load_symbol_to_eva(&device, "comms_buffer_start", &start_eva); 
-    if (err != HB_MC_SUCCESS) { 
-        fprintf(stderr, "hb_mc_loader_symbol_to_eva failed\n");
-        return err;
-    }
-
-    int32_t ready = 1;
-    int32_t start_idx = 0;
     int32_t int_size = sizeof(int32_t);
     for (int32_t i = 0; i < 4; i++) {
-        // Get the tile coordinate for each tile. Not that because the first row 
-        // is reserved for I/O, tile ID 0 corresponds to coordinate (0, 1) and
-        // so on. 
-        hb_mc_coordinate_t tile_coordinate = device.mesh->tiles[i].coord;
-
-        // For each tile, write a value to the start of the communication buffer
-        err = hb_mc_manycore_eva_write(device.mc, &default_map, &tile_coordinate,
-            &buffer_eva, &i, int_size);
-
-        // Then write the communication metadata values (array-based ones require
-        // pointer arithmetic on the EVA values):
-        // Ready flag
-        hb_mc_eva_t ready_offset_eva = ready_eva + i * int_size;
-        err |= hb_mc_manycore_eva_write(device.mc, &default_map, &tile_coordinate,
-            &ready_offset_eva, &ready, int_size);
-
-        // Start idx for each communication ID (use tile ID)
-        hb_mc_eva_t start_idx_offset_eva = start_idx_eva + i * int_size;
-        err |= hb_mc_manycore_eva_write(device.mc, &default_map, &tile_coordinate,
-            &start_idx_offset_eva, &start_idx, int_size); 
-
-        // Size for each communication ID (use tile ID)
-        hb_mc_eva_t size_offset_eva = size_eva + i * int_size;
-        err |= hb_mc_manycore_eva_write(device.mc, &default_map, &tile_coordinate,
-            &size_offset_eva, &int_size, int_size);  
-
-        // New start idx for that tile's buffer
-        err |= hb_mc_manycore_eva_write(device.mc, &default_map, &tile_coordinate,
-            &start_eva, &int_size, int_size);        
-
-        if (err != HB_MC_SUCCESS) { 
-            fprintf(stderr, "hb_mc_manycore_eva_write failed\n");
-            return err;
-        }  
+        // Send each core it's tile ID as an argument
+        send_argument(&i, int_size, i, i, &device);
     }
 
     // Run the function.
     err = hb_mc_device_tile_groups_execute(&device);
     if (err) return err;
 
-    // Lookup the return EVA on the last tile
-    hb_mc_eva_t global_return_eva; 
-    err = hb_mc_loader_symbol_to_eva(device.program->bin, device.program->bin_size,
-        "global_return", &global_return_eva); 
-    if (err != HB_MC_SUCCESS) { 
-        fprintf(stderr, "hb_mc_loader_symbol_to_eva failed\n");
-        return err;
-    }
-
-    hb_mc_coordinate_t host_coordinate = hb_mc_manycore_get_host_coordinate(device.mc); 
-    // EVA read the single return value
-    err = hb_mc_manycore_eva_read(device.mc, &default_map, &host_coordinate,
-        &global_return_eva, dest, int_size); 
-    if (err) {
-        fprintf(stderr, "hb_mc_device_memcpy to host failed\n");
-        return err;
-    }
+    // Get the return value, which should be a single int sum
+    receive_return(dest, int_size, &device);
 
     // Clean up.
     err = hb_mc_device_finish(&device);
